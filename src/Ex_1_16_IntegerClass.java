c48e5741e97507347e730168f073f95814f0a338
ik013043z1 <ik013043z1@H013043.NET>
2018-10-09 13:58:11 +0200

    1.0

---
+++ src/Ex_1_16_IntegerClass.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author ik013043z1
+ *
+ */import java.util.Scanner;
+ 	import java.lang.Integer;
+
+public class Ex_1_16_IntegerClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int number1;
+		int number2;
+		
+		int result;
+		
+		System.out.println("Enter two number to know if they are the same");
+		
+		number1 = scan.nextInt();
+		number2 = scan.nextInt();
+		
+		result = compareTo(number1, number2);
+	
+		
+		
+		
+		
+		
+
+	}
+
+	}
