d5b19d01570d9588bfdc8470bc6ee285f7cd80ba
XArtola <xartolaza18dw@ikzubirimanteo.com>
2018-10-12 13:36:04 +0200

    3.0

--- src/Ex_1_16_IntegerClass.java
+++ src/Ex_1_16_IntegerClass.java
@@ -25,7 +25,7 @@ public class Ex_1_16_IntegerClass {
 		
 		int result;
 		
-		System.out.println("Enter two number to know if they are the same");
+		System.out.println("Enter two number to know if they are the same\n");
 		
 		number1 = scan.nextInt();
 		number2 = scan.nextInt();
@@ -54,27 +54,82 @@ public class Ex_1_16_IntegerClass {
 				break;
 			case 0:
 				
-				System.out.println(number1+ " is the bigger as "+number2);
+				System.out.println(number1+ " is equal to "+number2);
 				break;
 			
 		}
 		
 		
 		/* getInteger()
-		 * 
-		 * 
+		 *  This command determines the integer value of the system property with the specified name.
+		 * In this case we can know the version of the JRE installed 32/64 bits
 		 */
-		System.out.println("Enter numbers:");
 		
-		String sentence;
-		int abc = 5;
+		System.out.println("Enter numbers:\n");
+		
+		String sentence = "sun.arch.data.model";
+		
+		System.out.println(Integer.getInteger(sentence));
+		
+		/* intValue()
+		 * Returns to an int the value of an integer object
+		 */
+		
+		number1 = num1.intValue();
+		
+		System.out.println(number1);
+		
+		/* parseInt()
+		 * This command convert a string value into an int
+		 */
+		
+		int number3;
+		
+		scan.nextLine();
+		
+		System.out.println("Enter numbers\n");
 		
 		sentence = scan.nextLine();
-		sentence = scan.nextLine();
 		
-		System.out.println(num1.getInteger(sentence));
+		number3 = Integer.parseInt(sentence);
+		
+		System.out.println(number3);
+		
+		/* shortValue()
+		 * This command returns the value of a Short type object to a short variable
+		 */
+		
+		short number4;
+		
+		System.out.println("Enter a short number:\n");
+		
+		Short shor = scan.nextShort();
+		
+		number4 = shor.shortValue();
+		
+		System.out.println(number4);
+		
+		/* toString()
+		 * This command converts the value of an object into a string
+		 */
+		
+		String sentence2;
+		
+		sentence = num1.toString();
+		sentence2 = shor.toString();
+		
+		System.out.println("The string value of the Integer object is: "+sentence+
+				"\n"+ "The string value of of the Short object is: " +sentence2);
 		
 		
+		/* valueOf()
+		 * This command takes the value of an object into another
+		 */
+		
+		Integer num2 = num1.valueOf(number4);
+		Integer num3 = Integer.valueOf(5);
+		
+		System.out.println(num2+"and"+num3);
 		
 	}
 
